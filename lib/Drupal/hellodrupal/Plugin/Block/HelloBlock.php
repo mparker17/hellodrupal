a554405db3111d0470254f50dac4c34a5f80fa3c
Larry Garfield <garfield@palantir.net>
2013-08-13 09:52:46 -0500

    Strip out to a block skeleton.

--- lib/Drupal/hellodrupal/Plugin/Block/HelloBlock.php
+++ lib/Drupal/hellodrupal/Plugin/Block/HelloBlock.php
@@ -15,7 +15,7 @@ use Drupal\Core\Annotation\Translation;
  * Provides a hello world block.
  *
  * @Plugin(
- *  id = "hellodrupal_hello",
+ *  id = "SOME_UNIQUE_VALUE",
  *  admin_label = @Translation("Hello World"),
  *  module = "hellodrupal"
  * )
@@ -26,39 +26,27 @@ class HelloBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function settings() {
-    return array(
-      'person' => 'World',
-    );
+
   }
 
   /**
    * {@inheritdoc}
    */
   public function blockForm($form, &$form_state) {
-    $form['person'] = array(
-      '#type' => 'textfield',
-      '#title' => t('Person'),
-      '#maxlength' => 50,
-      '#default_value' => $this->configuration['person'],
-      '#required' => TRUE,
-    );
-    return $form;
+
   }
 
   /**
    * {@inheritdoc}
    */
   public function blockSubmit($form, &$form_state) {
-    $this->configuration['person'] = $form_state['values']['person'];
+
   }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    return array(
-      '#theme' => 'hellodrupal_hello_block',
-      '#person' => $this->configuration['person'],
-    );
+
   }
 }
