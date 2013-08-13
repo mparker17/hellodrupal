8aa008967c99593c7f50d98152b949026bf487df
Larry Garfield <garfield@palantir.net>
2013-08-13 09:36:17 -0500

    Add additional controller examples.

--- lib/Drupal/hellodrupal/Controller/HelloController.php
+++ lib/Drupal/hellodrupal/Controller/HelloController.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\hellodrupal\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 use Drupal\Core\Controller\ControllerBase;
 
 /**
@@ -12,13 +14,35 @@ class HelloController extends ControllerBase {
   /**
    * Route callable method.
    *
+   * @return
+   *   A theme array. See hellodrupal-hello.html.twig.
+   */
+  public function hello() {
+    return "Hello World";
+  }
+
+  /**
+   * Route callable method.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   A JSON response.
+   */
+  public function helloPage() {
+    $json['hello'] = 'World';
+
+    return new JsonResponse($json);
+  }
+
+  /**
+   * Route callable method.
+   *
    * @param $person
    *   The name to display.
    *
    * @return
    *   A theme array. See hellodrupal-hello.html.twig.
    */
-  public function hello($person) {
+  public function helloPerson($person) {
     return array(
       '#theme' => 'hellodrupal_hello',
       '#person' => $person,
